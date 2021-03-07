9568977d1e6148715072c710923fd1ad1b075196
AHMED <eng.computer1@yahoo.com>
2021-03-07 02:13:24 -0600

    initial comments

---
+++ java-backend-crud/src/main/java/com/pack/main/JavaBackendCrudApplication.java
@@ -0,0 +1,13 @@
+package com.pack.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendCrudApplication.class, args);
+	}
+
+}
