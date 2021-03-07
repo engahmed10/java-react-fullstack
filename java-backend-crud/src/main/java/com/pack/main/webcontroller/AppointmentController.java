b45e3fe722d66aeafe84957bdcaadd30907d503a
AHMED <eng.computer1@yahoo.com>
2021-03-07 09:47:41 -0600

    created appointment controller and showed result in the browser

--- java-backend-crud/src/main/java/com/pack/main/webcontroller/AppointmentController.java
+++ java-backend-crud/src/main/java/com/pack/main/webcontroller/AppointmentController.java
@@ -1,5 +1,21 @@
 package com.pack.main.webcontroller;
 
-public class AppointmentController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.pack.main.domain.Appointment;
+import com.pack.main.repository.AppointmentRepository;
+
+@RestController   
+public class AppointmentController {
+	
+	@Autowired
+	private AppointmentRepository repository; 
+	
+	@RequestMapping("/appointments")
+	public Iterable <Appointment>  getAppointments(){
+		return repository.findAll();
+	}
+	
 }
