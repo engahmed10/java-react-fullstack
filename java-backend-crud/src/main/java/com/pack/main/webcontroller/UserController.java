53758af98533ef931fe516642ea7b4e03945bb61
AHMED <eng.computer1@yahoo.com>
2021-03-12 09:52:47 -0600

    request delete added to backend

--- java-backend-crud/src/main/java/com/pack/main/webcontroller/UserController.java
+++ java-backend-crud/src/main/java/com/pack/main/webcontroller/UserController.java
@@ -1,11 +1,16 @@
 package com.pack.main.webcontroller;
 
 
+import java.lang.System.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -33,6 +38,18 @@ public class UserController {
 		  repository.save(user);
 		return user;
 		
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Long> deleteUser(@PathVariable Long id) {
+		
+		try {
+			 repository.deleteById(id);
+			 return ResponseEntity.ok().build();
+		} catch (ResourceNotFoundException ex) {
+			// TODO: handle exception
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	
